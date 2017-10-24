950eb3080fff8513eadb59ea291fe55452e4c82d
juddw <juddwhitehead@gmail.com>
2017-10-24 14:54:40 -0400

    FinancialPortal Initial State

---
+++ jwhitehead-FinancialPortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jwhitehead_FinancialPortal.MvcApplication" Language="C#" %>
---
+++ jwhitehead-FinancialPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jwhitehead_FinancialPortal.Startup))]
+namespace jwhitehead_FinancialPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
